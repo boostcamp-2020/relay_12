d9ffb343b65477ab4e798884d5eb94ee2e14a09f
handhand <sohnjunior@naver.com>
2020-08-08 00:07:39 +0900

    feat: 게시글 수정 페이지

--- 2week_front/src/components/BoardView/Board.js
+++ 2week_front/src/components/BoardView/Board.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import { deletePost } from "../../lib/api";
 import { withRouter } from "react-router-dom";
+import { Link } from 'react-router-dom';
 import './style.css';
 
 const Board = (props) => {
 
-    const handleUpdate = () => {
-        // 상세 정보 수정 페이지로 이동 
-        console.log('수정');
-    }
-
     const handleRemove = async (e) => {
         e.preventDefault();
         try {
-            await deletePost(props.key);
+            await deletePost(props.postId);
         } catch(err) {
             console.error(err.message);
         }
@@ -21,21 +17,29 @@ const Board = (props) => {
 
     return (
         <>
-        <div className="BoardView">
-            <div className="title">
-                <h2>{props.title}</h2>
-            </div>
-            <div className="info">
-                <span>{props.username}</span>
-                <span>{props.createdAt}</span>
-            </div>
+            <div className="BoardView">
+                <div className="title">
+                    <h2>{props.title}</h2>
+                </div>
+                <div className="info">
+                    <span>{props.username}</span>
+                    <span>{props.createdAt}</span>
+                </div>
 
-            <div className="board">
-                <p>{props.body}</p>
+                <div className="board">
+                    <p>{props.body}</p>
+                </div>
             </div>
-        </div>
-        <button onClick={handleUpdate}>수정</button>
-        <button onClick={handleRemove}>삭제</button>
+            <Link to={{
+                pathname: `/posts/update/${props.postId}`,
+                state: {
+                    title: props.title,
+                    body: props.body
+                },
+            }}>
+                <button>수정</button>
+            </Link>
+            <button onClick={handleRemove}>삭제</button>
         </>
     );
 };
