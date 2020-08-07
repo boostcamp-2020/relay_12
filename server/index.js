9e5289b92188acd9f3b0674357d0ff14745af789
Wonseok Song <dnjstjr0517@gmail.com>
2020-08-07 17:20:06 +0900

    MOD DB 연결

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 1337;
+
+const express = require('express');
+const mysql = require('mysql');
+const dbconfig = require('./config/database.js');
+const connection = mysql.createConnection(dbconfig);
+
+const app = express();
+
+connection.query('SELECT * from user', (error, rows) => {
+    if (error) throw error;
+    console.log('User info is: ', rows);
+});
+
+
