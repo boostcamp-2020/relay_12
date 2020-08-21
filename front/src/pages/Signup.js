75dad7534f51d182fcd714f74988cae42601d094
Yu Taek <lcpnine@gmail.com>
2020-08-21 17:21:53 +0900

    frontend the final

--- front/src/pages/Signup.js
+++ front/src/pages/Signup.js
@@ -9,7 +9,7 @@ const Signup = () => {
   const [id, setId] = useState(""); // id
   const [password, setPassword] = useState(""); // pw
   const [school, setSchool] = useState(""); // school
-  const [hobby, setHobby] = useState("선택"); // interests
+  // const [hobby, setHobby] = useState([]); // interests
 
   const setNameText = e => {
     setName(e.target.value);
@@ -24,13 +24,11 @@ const Signup = () => {
   const setSchoolText = e => {
     setSchool(e.target.value);
   };
-  const setHobbyText = e => {
+  const setHobby = e => {
     setHobby(e.target.value);
   };
   const handlePost = (e) => {
-    console.log()
-    console.log('e');
-    console.log(e.target.id.value);
+    console.log(e.target.hobby);
     e.preventDefault();
     const formData = new FormData();
     formData.append("id", e.target.id.value);
@@ -85,13 +83,37 @@ const Signup = () => {
             onChange={setSchoolText}
           />
           <br />
-          <FormLabel htmlFor="hobby">취미</FormLabel>
-          <NativeSelect id="hobby" name="hobby" onChange={setHobbyText}>
-            <option value={hobby}>선택</option>
-            <option value="soccer">축구</option>
-            <option value="music">음악 감상</option>
-            <option value="movie">영화 감상</option>
-          </NativeSelect>
+          <FormLabel htmlFor="hobby">취미</FormLabel><br/>
+          <input type="checkbox" id="soccer" name="hobby" value="축구"></input>
+          <label for="hobby"> 축구 </label><br></br>
+          <input type="checkbox" id="basketball" name="hobby" value="농구"></input>
+          <label for="hobby"> 농구 </label><br></br>
+          <input type="checkbox" id="baseball" name="hobby" value="야구"></input>
+          <label for="hobby"> 야구 </label><br></br>
+          <input type="checkbox" id="movie" name="hobby" value="영화"></input>
+          <label for="hobby"> 영화 </label><br></br>
+          <input type="checkbox" id="music" name="hobby" value="노래"></input>
+          <label for="hobby"> 노래 </label><br></br>
+          <input type="checkbox" id="drawing" name="hobby" value="그림"></input>
+          <label for="hobby"> 그림 </label><br></br>
+          <input type="checkbox" id="PC game" name="hobby" value="PC게임"></input>
+          <label for="hobby"> PC게임 </label><br></br>
+          <input type="checkbox" id="exhibition" name="hobby" value="전시회"></input>
+          <label for="hobby"> 전시회 </label><br></br>
+          <input type="checkbox" id="console" name="hobby" value="콘솔게임"></input>
+          <label for="hobby"> 콘솔게임 </label><br></br>
+          <input type="checkbox" id="hiphop" name="hobby" value="힙합"></input>
+          <label for="hobby"> 힙합 </label><br></br>
+          <input type="checkbox" id="groumet" name="hobby" value="맛집"></input>
+          <label for="hobby"> 맛집 </label><br></br>
+          <input type="checkbox" id="book" name="hobby" value="독서"></input>
+          <label for="hobby"> 독서 </label><br></br>
+          <input type="checkbox" id="studying" name="hobby" value="공부"></input>
+          <label for="hobby"> 공부 </label><br></br>
+          <input type="checkbox" id="dancing" name="hobby" value="춤"></input>
+          <label for="hobby"> 춤 </label><br></br>
+          <input type="checkbox" id="gym" name="hobby" value="헬스"></input>
+          <label for="hobby"> 헬스 </label><br></br>
           <br />
           <Input type="submit" value="저장"/>
         </form>
